e10f524b0a75e095a34e624f9e4f6313e2943578
Jazdot <riswanmp6@gmail.com>
2023-12-18 12:13:25 +0530

    Your commit message

---
+++ KDCR_Mobile_Robot/catkin_ws/devel_isolated/env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+# generated from catkin.builder Python module
+
+/home/richu/KDCR_Mobile_Robot/catkin_ws/devel_isolated/multi_robot_scenario/env.sh "$@"
---
+++ KDCR_Mobile_Robot/catkin_ws/devel_isolated/setup.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+# generated from catkin.builder Python module
+
+. "/home/richu/KDCR_Mobile_Robot/catkin_ws/devel_isolated/multi_robot_scenario/setup.zsh"
